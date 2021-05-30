e20508cfe6b9a3e6b5d2e529d7eedd9da51955ee
Maalim18 <ali.mohamed@student.moringaschool.com>
2021-05-30 19:25:47 +0300

    generate credentials class nd create a constructor

---
+++ credentials.py
@@ -0,0 +1,18 @@
+class Credentials:
+
+    '''
+    class that generates new instance credentials
+    '''
+
+    def __init__(self, account_name, account_password):
+        self.account_name = account_name
+        self.account_password = account_password
+
+    credentials_list = []
+
+    def save_credentials(self):
+        '''
+        method that saves credentials objects into credentials list 
+        '''
+
+        self.credentials_list.append(self)

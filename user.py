3020ea2390dcd2cfee5e7571ab2a00d668881702
Maalim18 <ali.mohamed@student.moringaschool.com>
2021-05-31 09:17:07 +0300

    import pyper clip

--- user.py
+++ user.py
@@ -1,18 +1,20 @@
+import pyperclip
+
+
+
 class User:
-
     '''
-    class that generates new instance of user
+    class that allows user to generate user account
     '''
-
+    
     user_list = []
-
-    def __init__(self, user_name, password):
+    
+    def __init__(self,user_name,password):
         self.user_name = user_name
         self.password = password
-
+        
     def save_user(self):
         '''
-        save_user method saves a new user objects to the user_list
+        save_user method that saves new user object
         '''
-
         User.user_list.append(self)
